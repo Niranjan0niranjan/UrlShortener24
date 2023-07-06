333354d61a317e4f9140d06bb5df480fa86ba404
Niran <niranjansaha1081@gmail.com>
2023-07-06 22:58:05 +0530

    Updates 1

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const mongoose = require('mongoose');
+const app = express();
+const PORT = process.env.PORT|| 3000;
+
+//Set up default mongoose connection
+const mongoDB = 'mongodb://localhost:27017/UrlShortener';
+
+mongoose.connect(mongoDB, { 
+    useNewUrlParser: true, useUnifiedTopology: true
+});
+
+//Get the default connection
+var db = mongoose.connection;
+//Bind connection to error event (to get notification of connection errors)
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) =>{
+    res.render('index');
+});
+
+app.post('/shortUrls', (req, resd) =>{
+    
+})
+
+app.listen(PORT, () => {
+    console.log(`Express app is listening on port ${PORT}`);
+});
